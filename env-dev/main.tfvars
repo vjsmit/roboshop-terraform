23712612d929dd1d06030ffe53949a68577415a7
vjsmit <vj.smit97@gmail.com>
2023-08-04 00:02:39 +0530

    updating userdata.sh

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -47,12 +47,21 @@ rds = {
 
 documentdb = {
   main = {
-    component               = "docdb"
+    component         = "docdb"
+    engine            = "docdb"
+    engine_version    = "4.0.0"
+    db_instance_count = 1
+    instance_class    = "db.t3.medium"
   }
 }
 
 elasticache = {
   main = {
-    component               = "elasticache"
+    component    = "elasticache"
+    engine       = "redis"
+    engine_version = "6.2"
+    replicas_per_node_group = 1
+    num_node_groups = 1
+    node_type       = "cache.t3.micro"
   }
 }
--- main.tf
+++ main.tf
@@ -61,11 +61,17 @@ module "documentdb" {
   for_each = var.documentdb
   component = each.value["component"]
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  engine            = each.value["engine"]
+  engine_version    = each.value["engine_version"]
+  db_instance_count = each.value["db_instance_count"]
+  instance_class    = each.value["instance_class"]
 
   tags= var.tags
   env= var.env
   kms_key_arn = var.kms_key_arn
-  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+
 }
 
 module "elasticache" {
@@ -73,9 +79,15 @@ module "elasticache" {
   for_each = var.elasticache
   component = each.value["component"]
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+  num_node_groups = each.value["num_node_groups"]
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  node_type = each.value["node_type"]
 
   tags= var.tags
   env= var.env
   kms_key_arn = var.kms_key_arn
-  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
 }
