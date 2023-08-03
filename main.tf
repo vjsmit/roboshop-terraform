0b9ca632e082a60155cd874b77e4cd570fe3298f
vjsmit <vj.smit97@gmail.com>
2023-08-03 21:13:14 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -11,46 +11,67 @@ module "vpc" {
   default_vpc_rt = var.default_vpc_rt
 }
 
-module "app_server" {
-  source = "git::https://github.com/vjsmit/tf-module-app.git"
-  env        = var.env
-  tags       = var.tags
-  component  = "test"
-  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "app", null), "subnet_ids", null)[0]
-  vpc_id     = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-}
+#module "app_server" {
+#  source = "git::https://github.com/vjsmit/tf-module-app.git"
+#  env        = var.env
+#  tags       = var.tags
+#  component  = "test"
+#  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "app", null), "subnet_ids", null)[0]
+#  vpc_id     = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#}
 
-module "rabbbitmq" {
-  source = "git::https://github.com/vjsmit/tf-module-rabbitmq.git"
-  for_each = var.rabbitmq
+#module "rabbbitmq" {
+#  source = "git::https://github.com/vjsmit/tf-module-rabbitmq.git"
+#  for_each = var.rabbitmq
+#  component = each.value["component"]
+#  instance_type = each.value["instance_type"]
+#
+#  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+#  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)[0]
+#
+#  env = var.env
+#  tags = var.tags
+#  allow_ssh_cidr = var.allow_ssh_cidr
+#  zone_id = var.zone_id
+#  kms_key_id = var.kms_key_id
+#}
+
+#module "rds" {
+#  source = "git::https://github.com/vjsmit/tf-module-rds.git"
+#   for_each = var.rds
+#   component= each.value["component"]
+#   engine= each.value["engine"]
+#   engine_version= each.value["engine_version"]
+#   db_name= each.value["db_name"]
+#   subnet_ids= lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)
+#   instance_count = each.value["instance_count"]
+#   instance_class = each.value["instance_class"]
+#   vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+#
+#   tags= var.tags
+#   env= var.env
+#   kms_key_arn = var.kms_key_arn
+#}
+
+module "documentdb" {
+  source = "git::https://github.com/vjsmit/tf-module-documentdb.git"
+  for_each = var.documentdb
   component = each.value["component"]
-  instance_type = each.value["instance_type"]
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)
 
-  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)[0]
-
-  env = var.env
-  tags = var.tags
-  allow_ssh_cidr = var.allow_ssh_cidr
-  zone_id = var.zone_id
-  kms_key_id = var.kms_key_id
+  tags= var.tags
+  env= var.env
+  kms_key_arn = var.kms_key_arn
 }
 
-module "rds" {
-  source = "git::https://github.com/vjsmit/tf-module-rds.git"
-   for_each = var.rds
-   component= each.value["component"]
-   engine= each.value["engine"]
-   engine_version= each.value["engine_version"]
-   db_name= each.value["db_name"]
-   subnet_ids= lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets_id", null), "db", null), "subnet_ids", null)
-   instance_count = each.value["instance_count"]
-   instance_class = each.value["instance_class"]
-   vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_subnets_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-
-   tags= var.tags
-   env= var.env
-   kms_key_arn = var.kms_key_arn
-}
+#module "rds" {
+#  source = "git::https://github.com/vjsmit/tf-module-elasticache.git"
+#  for_each = var.elasticache
+#
+#
+#  tags= var.tags
+#  env= var.env
+#  kms_key_arn = var.kms_key_arn
+#}
